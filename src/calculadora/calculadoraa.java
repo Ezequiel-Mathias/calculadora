9edb802042131ac9002bc4592d71a42d0ff7ebb9
EZEQUIEL MATHIAS DE OLIVEIRA PEREIRA <21276325@CFP127ALUNO.LOCAL>
2021-09-03 16:59:48 -0300

    calculadora finalizado

---
+++ src/calculadora/calculadoraa.java
@@ -0,0 +1,29 @@
+package calculadora;
+
+import java.util.Scanner;
+
+public class calculadoraa {
+
+	public static void main(String[] args) {
+		
+		Scanner leitor = new Scanner (System.in);
+		int multiplicador = 0;
+		int numero;
+		int total;
+		
+		System.out.println("Qual calculadora deseja ver ? ");
+        numero = leitor.nextInt();
+        
+        
+        while ( multiplicador <= 10) {
+			  
+			 total = numero * multiplicador;
+			 
+			 System.out.println(numero + " x " + multiplicador + " = "+ total); 
+			
+        	multiplicador  = multiplicador  + 1;
+			
+		}
+	}
+
+}
